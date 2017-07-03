f1af2dd522bf0d74a0e2c54ea52b0608c301c3f1
lmncomeon <540936101@qq.com>
2017-07-03 16:44:53 +0800

    增加购物车效果

--- PersonalCenterDemo/PersonalCenterDemo/Project/Controllers/KTV字母效果/KTVCaptionViewController.m
+++ PersonalCenterDemo/PersonalCenterDemo/Project/Controllers/KTV字母效果/KTVCaptionViewController.m
@@ -44,6 +44,7 @@
             show.width = background.size.width;
         }];
     });
+    
 }
 
 @end
--- PersonalCenterDemo/PersonalCenterDemo/Project/Controllers/OneViewController.m
+++ PersonalCenterDemo/PersonalCenterDemo/Project/Controllers/OneViewController.m
@@ -14,6 +14,10 @@
 @property (nonatomic, strong) UITableView *mainTableView;
 @property (nonatomic, strong) NSMutableArray <ControllerModel *> *dataList;
 
+@property (nonatomic, strong) UIView *backView;
+@property (nonatomic, strong) UIImageView *tempimgView;
+@property (nonatomic, strong) UIView *bottomView;
+
 @end
 
 @implementation OneViewController
@@ -21,7 +25,7 @@
 - (UITableView *)mainTableView {
     if (!_mainTableView) {
         _mainTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, (kScreenHeight - 64)) style:UITableViewStyleGrouped];
-        _mainTableView.backgroundColor = [UIColor groupTableViewBackgroundColor];
+        _mainTableView.backgroundColor = [UIColor clearColor];
         _mainTableView.dataSource      = self;
         _mainTableView.delegate  = self;
         [self.view addSubview:_mainTableView];
@@ -32,6 +36,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    
+    SDKCustomLabel *lab = [SDKCustomLabel setLabelTitle:@"测试" setLabelFrame:CGRectMake(0, 0, kScreenWidth, adaptY(20)) setLabelColor:commonBlueColor setLabelFont:kFont(12)];
+    lab.textAlignment = 1;
+    [self.view addSubview:lab];
     
     [self mainTableView];
 }
@@ -58,6 +68,10 @@
     return cell;
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 0.01f;
+}
+
 #pragma mark - tableview delegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
@@ -67,10 +81,57 @@
         SDKBaseViewController *vc = [class new];
         vc.hidesBottomBarWhenPushed = YES;
         [self.navigationController pushViewController:vc animated:true];
+    } else {
+        // 购物车效果
+        [self addShoppingCartEffect];
     }
 }
 
+- (void)addShoppingCartEffect {
+    // 背景view
+    self.backView = [[UIView alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    self.backView.backgroundColor = [UIColor orangeColor];
+    [[UIApplication sharedApplication].keyWindow addSubview:self.backView];
+    
+    
+    
+    // 截取当前屏幕的img
+    UIGraphicsBeginImageContextWithOptions([UIScreen mainScreen].bounds.size, NO, [UIScreen mainScreen].scale);
+    [self.view.window drawViewHierarchyInRect:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height) afterScreenUpdates:NO];
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    // 截取的屏幕
+    self.tempimgView = [[UIImageView alloc]initWithFrame:self.backView.bounds];
+    self.tempimgView.image = img;
+    [self.backView addSubview:self.tempimgView];
+    // 遮罩层
+    UIView *maskView = [[UIView alloc]initWithFrame:self.backView.bounds];
+    maskView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.2];
+    [maskView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTouchMaskView)]];
+    [self.backView addSubview:maskView];
+    
+    // 弹出的view
+    self.bottomView = [[UIView alloc]initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height,  [UIScreen mainScreen].bounds.size.width, 200)];
+    self.bottomView.backgroundColor = [UIColor redColor];
+    [self.backView addSubview:self.bottomView];
+    
+    // 动画
+    [UIView animateWithDuration: 0.35 animations: ^{
+        self.tempimgView.transform = CGAffineTransformScale(CGAffineTransformIdentity,0.8,0.8);
+        self.tempimgView.y = adaptY(10);
+        self.bottomView.frame = CGRectMake(0, kScreenHeight-adaptY(200), kScreenWidth, adaptY(200));
+    } completion: nil];
+}
 
+-(void)didTouchMaskView {
+    [UIView animateWithDuration: 0.35 animations: ^{
+        self.tempimgView.transform = CGAffineTransformScale(CGAffineTransformIdentity,1,1);
+        self.tempimgView.y = 0;
+        self.bottomView.y = kScreenHeight;
+    } completion:^(BOOL finished) {
+        [self.backView removeFromSuperview];
+    }];
+}
 
 #pragma mark - lazy load
 - (NSMutableArray <ControllerModel *> *)dataList {
@@ -82,8 +143,7 @@
         [_dataList addObject:[ControllerModel modelWithTitle:@"右拉加载" className:@"RightLoadViewController"]];
         [_dataList addObject:[ControllerModel modelWithTitle:@"物流" className:@"LogisticsViewController"]];
         [_dataList addObject:[ControllerModel modelWithTitle:@"KTV字幕建议实现" className:@"KTVCaptionViewController"]];
-        
-        
+        [_dataList addObject:[ControllerModel modelWithTitle:@"购物车效果" className:@""]];
     }
     return _dataList;
 }
